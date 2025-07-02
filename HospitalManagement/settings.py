6f264b2ca4f38a679405b26f01e8302d86e64900
Bhagyashree Chouhan <chouhanbhagyashree123@gmail.com>
2025-07-02 12:24:07 +0530

    Configure project to use PostgreSQL with dj_database_url

--- HospitalManagement/settings.py
+++ HospitalManagement/settings.py
@@ -78,13 +78,14 @@ WSGI_APPLICATION = 'HospitalManagement.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+import dj_database_url
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3'
+    )
 }
+
 
 
 # Password validation
