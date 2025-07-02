91baf5b1f730480f2f02970ed91e281f98781183
Bhagyashree Chouhan <chouhanbhagyashree123@gmail.com>
2025-07-02 15:44:23 +0530

    Add all models and migrations

--- core/models.py
+++ core/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    ROLE_CHOICES = (('Doctor', 'Doctor'), ('Admin', 'Admin'))
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES)
+
+class Patient(models.Model):
+    GENDER_CHOICES = (('Male', 'Male'), ('Female', 'Female'), ('Other', 'Other'))
+    name = models.CharField(max_length=100)
+    age = models.PositiveIntegerField()
+    gender = models.CharField(max_length=6, choices=GENDER_CHOICES)
+    address = models.TextField()
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class MedicalRecord(models.Model):
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE, related_name='records')
+    symptoms  = models.TextField()
+    diagnosis  = models.TextField()
+    treatment  = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
